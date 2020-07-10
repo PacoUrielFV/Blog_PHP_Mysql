887b11e506008d39c5c9dad5c27b9eeec0d3c7ac
Paco Flores <pacoflores1995@gmail.com>
2020-07-09 21:12:04 -0500

    Se crea el blog

---
+++ admin/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('RUTA', 'http://localhost/practicas/Blog_PHP_Mysql');
+
+$bd_config = array(
+    'basedatos' => 'blog_practica',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$blog_config = array (
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'imagenes/'
+);
+
+$blog_admin = array(
+    'usuario' => 'Paco',
+    'password' => '123'
+);
+
+?>
---
+++ views/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p class="copyright">Copyright 2020 - 2023 Equipo 3</p>
+    </footer>
+</body>
+</html>
